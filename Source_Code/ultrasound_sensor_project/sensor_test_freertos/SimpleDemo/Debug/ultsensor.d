59422218dd004429f8aa224fe7dc9f2f318787ad
sujeeth emmadi <sujeeth emmadi>
2017-03-20 11:43:50 -0700

    *added ultrasonic sensor freertos proj

---
+++ Source_Code/ultrasound_sensor_project/sensor_test_freertos/SimpleDemo/Debug/ultsensor.d
@@ -0,0 +1,42 @@
+ultsensor.o ultsensor.d: ../ultsensor.c ../ultsensor.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/FreeRTOS.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/projdefs.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\SimpleDemo/FreeRTOSConfig.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\CMSISv1p30_LPC17xx\inc/core_cm3.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/portable.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\portable/portmacro.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/mpu_wrappers.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/task.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/list.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/semphr.h \
+ C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/queue.h
+
+../ultsensor.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/FreeRTOS.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/projdefs.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\SimpleDemo/FreeRTOSConfig.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\CMSISv1p30_LPC17xx\inc/core_cm3.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/portable.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\portable/portmacro.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/mpu_wrappers.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/task.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/list.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/semphr.h:
+
+C:\Users\sujee\Desktop\ARC\SJSU_Coursework\Spring_2017\295B\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\FreeRTOS-Demo-and-Docs-for-the-LPCXpresso-LPC1768\SourceCode\FreeRTOS-simple-demo-for-LPCXpresso\FreeRTOS_Library\include/queue.h:
