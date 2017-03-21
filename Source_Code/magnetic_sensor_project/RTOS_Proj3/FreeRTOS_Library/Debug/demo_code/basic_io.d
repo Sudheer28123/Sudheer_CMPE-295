cd0d5730ca89dec2c9c942ca784928db029547a1
Sudheer28123 <sudheer2b1@gmail.com>
2017-03-21 00:27:37 -0700

    RTOS version of Magnetic sensor is implemented and project uploaded.

---
+++ Source_Code/magnetic_sensor_project/RTOS_Proj3/FreeRTOS_Library/Debug/demo_code/basic_io.d
@@ -0,0 +1,37 @@
+demo_code/basic_io.o demo_code/basic_io.d: ../demo_code/basic_io.c \
+ ../demo_code/consoleprint.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/FreeRTOS.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/projdefs.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/SimpleDemo/FreeRTOSConfig.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/LPC17xx.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/core_cm3.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/portable.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/portable/portmacro.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/mpu_wrappers.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/task.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/list.h
+
+../demo_code/consoleprint.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/FreeRTOS.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/projdefs.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/SimpleDemo/FreeRTOSConfig.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/LPC17xx.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/core_cm3.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/portable.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/portable/portmacro.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/mpu_wrappers.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/task.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/list.h:
---
+++ Source_Code/magnetic_sensor_project/RTOS_Proj3/FreeRTOS_Library/Debug/portable/port.d
@@ -0,0 +1,34 @@
+portable/port.o portable/port.d: ../portable/port.c \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/FreeRTOS.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/projdefs.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/SimpleDemo/FreeRTOSConfig.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/LPC17xx.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/core_cm3.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/portable.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/portable/portmacro.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/mpu_wrappers.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/task.h \
+ /Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/list.h
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/FreeRTOS.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/projdefs.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/SimpleDemo/FreeRTOSConfig.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/LPC17xx.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/core_cm3.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/portable.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/portable/portmacro.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/mpu_wrappers.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/task.h:
+
+/Users/sudheerdoddapaneni/Documents/Sudheer/Sem4/Project/LPCXpresso/RTOS_Proj3/FreeRTOS_Library/include/list.h:
