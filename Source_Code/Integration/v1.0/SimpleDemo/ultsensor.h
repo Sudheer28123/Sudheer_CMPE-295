0242c8649de3b150afe48ee4782d725c74c6ad67
sujeeth emmadi <sujeeth emmadi>
2017-03-21 19:04:02 -0700

    *Integration v1.0 - Sujeeth

---
+++ Source_Code/Integration/v1.0/SimpleDemo/ultsensor.h
@@ -0,0 +1,66 @@
+/*
+*Created March 10th, 2017
+*By Sujeeth Krishna Emmadi
+*
+*/
+
+#ifndef	ULTSENSOR_H_
+#define ULTSENSOR_H_
+
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "semphr.h"
+#include "portmacro.h"
+
+#define		configULT_TASK_PRIORITY				( tskIDLE_PRIORITY + 1 )
+
+#define configULT_FREQ					( 500 / portTICK_RATE_MS )
+
+//Bits to set up Timer0 and Timer1
+#define SBIT_TIMER0  1
+#define SBIT_TIMER1	 2
+
+//Bits to set in MCR that generate interrupt and clear MR0 register
+#define SBIT_MR0I    0
+#define SBIT_MR0R    1
+
+//Bit to enable counter
+#define SBIT_CNTEN   0
+
+//Bits to enable pclks
+#define PCLK_TIMER0  2
+#define PCLK_TIMER1	 4
+
+typedef enum { false, true } bool;
+
+struct USensor
+{
+	uint32_t start_time;
+	uint32_t stop_time;
+	bool flag1;
+	bool flag2;
+	bool flag3;
+	bool rflag;
+};
+
+struct USensor Ult;
+
+void TIMER0_IRQHandler(void);
+
+void EINT3_IRQHandler(void);
+
+unsigned int getPrescalarForUs(uint8_t timerPclkBit);
+
+void delay_us(int d_time);
+
+void ULTSEN_init();
+
+void ULTSEN();
+
+void ULTSEN_Task(void *UltSenParam1);
+
+
+#endif
