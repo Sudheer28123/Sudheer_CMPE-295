830ec515ae5cacaa42c7eecffae6c8bc74b4af55
VeenaManasa <veena3094@gmail.com>
2017-04-16 00:04:15 -0700

    added RTOS version of MODBUS

---
+++ Source_code/modbus/Modbus_FreeRTOS/SimpleDemo/Debug/mb.d
@@ -0,0 +1,34 @@
+mb.o mb.d: ../mb.c \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/port.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mb.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/port.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbport.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbproto.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbconfig.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbframe.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbproto.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbfunc.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbport.h \
+ C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbrtu.h
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/port.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mb.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/port.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbport.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbproto.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbconfig.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbframe.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbproto.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbfunc.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbport.h:
+
+C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include/mbrtu.h:
---
+++ Source_code/modbus/Modbus_FreeRTOS/SimpleDemo/Debug/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../cr_startup_lpc17.c \
+../main.c \
+../mb.c \
+../mbfunccoils.c \
+../mbfuncdisc.c \
+../mbfuncholding.c \
+../mbfuncinput.c 
+
+OBJS += \
+./cr_startup_lpc17.o \
+./main.o \
+./mb.o \
+./mbfunccoils.o \
+./mbfuncdisc.o \
+./mbfuncholding.o \
+./mbfuncinput.o 
+
+C_DEPS += \
+./cr_startup_lpc17.d \
+./main.d \
+./mb.d \
+./mbfunccoils.d \
+./mbfuncdisc.d \
+./mbfuncholding.d \
+./mbfuncinput.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -DDEBUG -D__USE_CMSIS=CMSISv1p30_LPC17xx -D__CODE_RED -D__REDLIB__ -I"C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo" -I"C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\SimpleDemo\include" -I"C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\CMSISv1p30_LPC17xx\inc" -I"C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\FreeRTOS_Library\include" -I"C:\Users\veena\Documents\LPCXpresso_8.1.0_597\workspace\FreeRTOS_Library\portable" -O0 -g3 -fsigned-char -c -fmessage-length=0 -fno-builtin -ffunction-sections -mcpu=cortex-m3 -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
